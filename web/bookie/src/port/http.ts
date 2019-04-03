26d3e879cde71f8d7a8ad4bd0fa719e481bf7619
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-04-03 22:29:14 +0800

    引入 “Axios”。

---
+++ web/bookie/src/port/http.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import QS from 'qs'
+
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = 'https://www.baidu.com'
+} else if (process.env.NODE_ENV === 'debug') {
+  axios.defaults.baseURL = 'https://www.ceshi.com'
+} else if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'https://www.production.com'
+}
+
+// 10s = timeout.
+axios.defaults.timeout = 10000
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+
+axios.defaults.withCredentials = true
