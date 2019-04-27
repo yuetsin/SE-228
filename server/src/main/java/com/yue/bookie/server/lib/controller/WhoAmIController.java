e30685cab1130e120c2b2a00b81fe2f0c9837441
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-04-27 18:15:47 +0800

    更新问好系统。

---
+++ server/src/main/java/com/yue/bookie/server/lib/controller/WhoAmIController.java
@@ -0,0 +1,4 @@
+package com.yue.bookie.server.lib.controller;
+
+public class WhoAmIController {
+}
--- server/src/main/java/com/yue/bookie/server/security/controller/PageController.java
+++ server/src/main/java/com/yue/bookie/server/security/controller/PageController.java
@@ -1,9 +1,13 @@
 package com.yue.bookie.server.security.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * 请求页面分发，注意和WebMvcConfig的对比，功能类似
  * @author Veiking
@@ -20,5 +24,13 @@ public class PageController {
 	@RequestMapping("/hello")
 	public String hello(Model model, String tt) {
 		return "hello";
+	}
+
+
+	@RequestMapping("/me")
+	@ResponseBody
+	public String me(Model model, String tt) {
+		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		return "Hello! You're " + userDetails.getUsername();
 	}
 }
