469409eaa469e41d366c716dbfef94c1d50c6603
yuetsin <akaza_akari@sjtu.edu.cn>
2019-06-29 20:26:58 +0800

    [fix] change initializer as '.py'

--- mongo/<example> init_mongo.sh
+++ mongo/<example> init_mongo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-mongo "mongodb+srv://cluster0-hol3x.mongodb.net/bookie_db" --username <replace with your username> --password <replace with your password>  "./bookie_initializer.js"
+mongo "mongodb+srv://cluster0-hol3x.mongodb.net/bookie_db" --username <replace with your username> --password <replace with your password>  "./bookie_initializer.py"
