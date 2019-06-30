a0cbbe7337fdfad361c14c944f6de9ac121a90dd
yuetsin <akaza_akari@sjtu.edu.cn>
2019-06-30 15:25:11 +0800

    [fix] new docker-compose

--- dockerize/sql-docker/script/privileges.sql
+++ dockerize/sql-docker/script/privileges.sql
@@ -3,6 +3,6 @@ select host, user from user;
 
 create user docker identified by '123456docker';
 
-grant all on bookie.* to docker@'%' identified by '123456docker' with grant option;
+grant all on bookie.* to docker@'47.103.15.32' identified by '123456docker' with grant option;
 
 flush privileges;
