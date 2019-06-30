baabd01d3627baf7af0c0844d9ea30dfcdb22cf1
yuetsin <akaza_akari@sjtu.edu.cn>
2019-06-30 19:04:35 +0800

    [add] iOS App skeleton

---
+++ iOS/Bookie/Bookie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bookie
+//
+//  Created by 法好 on 2019/6/30.
+//  Copyright © 2019 yuetsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
